beaa639bfce5a441b461b79c1ed0b4c4eaf15b07
ZEvans1 <zte.zachary@gmail.com>
2018-03-23 14:11:59 -0700

    Implement an implicit intent to navigate to the nutritionix website.

--- app/src/main/java/com/example/guest/healthapp/ui/AboutActivity.java
+++ app/src/main/java/com/example/guest/healthapp/ui/AboutActivity.java
@@ -1,16 +1,39 @@
 package com.example.guest.healthapp.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.guest.healthapp.R;
 
-public class AboutActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.aboutTextView) TextView mAboutTextView;
+    @BindView(R.id.websiteTextView) TextView mWebsiteTextView;
+
+    private String websiteURL = "https://www.nutritionix.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
 
+        mWebsiteTextView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mWebsiteTextView) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(websiteURL));
+            startActivity(webIntent);
+        }
     }
 }
